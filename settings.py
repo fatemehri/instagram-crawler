8de90a81e2064ff943b3ac255433b1c06221c683
Arman Yazdani <arman.yazdani.10@gmail.com>
2019-10-20 18:42:30 +0330

    Added new settings

--- settings.py
+++ settings.py
@@ -16,24 +16,24 @@ NEWSPIDER_MODULE = 'web_crawler.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'web_crawler (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -88,6 +88,17 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# LOGGING
+LOG_ENABLED = True
+LOG_FILE = '/var/log/scrapy/scrapy.log'
+LOG_LEVEL = 'INFO'
+
+# MEMORY USAGE
+MEMUSAGE_LIMIT_MB = 512
+
+# JOB DIRECTORY
+JOBDIR = '/opt/web_crawler/scrapy-data'
 
 MONGO_URI = ''
 MONGO_DATABASE = ''
